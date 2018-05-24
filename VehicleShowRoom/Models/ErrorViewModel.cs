0c7c39f8e1bda66a903ff530eb445ad0f0efcde8
J.Chris Findlay <j.chris.findlay@gmail.com>
2018-05-25 02:15:38 +1200

    Frontend template, post cleanup.  Got interrupted by a sick kid..

---
+++ VehicleShowRoom/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VehicleShowRoom.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
