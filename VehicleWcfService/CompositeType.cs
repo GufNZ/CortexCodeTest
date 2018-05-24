bc2bccd9b2bb79cdae859b24edad73a7c0031d7d
J.Chris Findlay <j.chris.findlay@gmail.com>
2018-05-24 20:37:33 +1200

    Initial cleanup

---
+++ VehicleWcfService/CompositeType.cs
@@ -0,0 +1,15 @@
+using System.Runtime.Serialization;
+
+namespace VehicleWcfService
+{
+	// Use a data contract as illustrated in the sample below to add composite types to service operations.
+	[DataContract]
+	public class CompositeType
+	{
+		[DataMember]
+		public bool BoolValue { get; set; } = true;
+
+		[DataMember]
+		public string StringValue { get; set; } = "Hello ";
+	}
+}
